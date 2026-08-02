d8a47745e63aa13cbc5849a799a0ebb7660d8b48
Jafor Shadik <jaforshadik60@gmail.com>
2026-08-02 18:25:40 +0600

    Improve main file

--- src/main.py
+++ src/main.py
@@ -3,8 +3,11 @@ print('Name : Jafor Shadik')
 today = date.today()
 print("Today's Date :",today)
 
-from utils import add, subtract
+from utils import add, subtract, mul, div
 num1 = 10
 num2 = 5
 print("Addition:", add(num1, num2))
 print("Subtraction:", subtract(num1, num2))
+print("Multiplication:", mul(num1, num2))
+print("Division:", div(num1, num2))
+
