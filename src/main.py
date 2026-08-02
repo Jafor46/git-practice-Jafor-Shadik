206dbbaad172333964dafa8e1c5b2834e07e6def
Jafor Shadik <jaforshadik60@gmail.com>
2026-08-02 18:06:49 +0600

    Add basic calculator functions

--- src/main.py
+++ src/main.py
@@ -2,3 +2,9 @@ from datetime import date
 print('Name : Jafor Shadik')
 today = date.today()
 print("Today's Date :",today)
+
+from utils import add, subtract
+num1 = 10
+num2 = 5
+print("Addition:", add(num1, num2))
+print("Subtraction:", subtract(num1, num2))
