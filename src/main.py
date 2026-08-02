ba3d1cc4e8bcb8d833804ef40e0fef3064f9cb9b
Jafor Shadik <jaforshadik60@gmail.com>
2026-08-02 18:35:50 +0600

    Operate the ne functions

--- src/main.py
+++ src/main.py
@@ -4,10 +4,18 @@ today = date.today()
 print("Today's Date :",today)
 
 from utils import add, subtract, mul, div
-num1 = 10
-num2 = 5
-print("Addition:", add(num1, num2))
-print("Subtraction:", subtract(num1, num2))
-print("Multiplication:", mul(num1, num2))
-print("Division:", div(num1, num2))
+try:
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+
+    print("Addition:", add(num1, num2))
+    print("Subtraction:", subtract(num1, num2))
+    print("Multiplication:", mul(num1, num2))
+    print("Division:", div(num1, num2))
+
+except ValueError as error:
+    print("Error:", error)
+
+except Exception as error:
+    print("Unexpected error:", error)
 
